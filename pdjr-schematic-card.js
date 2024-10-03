bdaac30863eb6ebd87b2e6082adc818260e8c8b8
Paul Reeve <preeve@pdjr.eu>
2024-10-03 12:10:35 +0200

    Update pdjr-schematic-card.js

--- pdjr-schematic-card.js
+++ pdjr-schematic-card.js
@@ -19,17 +19,12 @@ class ActiveDrawing extends HTMLElement {
             });
             props.appliedClass = cl;
           }
-          if (props.updateText) {
+          if (props.updateText !== undefined) {
             var text = this.myHass.states[id].state + (this.myHass.states[id]["attributes"]["unit_of_measurement"] || "");
-            props.elements.forEach((element) => {
-              element.textContent = text;
-            });
+            this.setText(props.elements, text);
           }
-          if (props.updateRotation) {
-            var text = `rotate(${this.myHass.states[id].state})`;
-            props.elements.forEach((element) => {
-              element.setAttribute('transform', text);
-            });
+          if (props.updateAttribute !== undefined) {
+            this.setAttribute(props.elements, props.updateAttribute.name, props.updateAttribute.value, this.myHass.states[id].state);
           }
         }
       });
@@ -59,13 +54,20 @@ class ActiveDrawing extends HTMLElement {
 
       group.entities.forEach((entity) => {
         let elems = getElements(entity.elements);
+        // Apply set_ actions...
+        if ("actions" in group) {
+          if ('set_class' in group.actions) setClass(elems, group.actions.set_class);
+          if ('set_text' in group.actions) setText(elems, group.actions.set_text);
+          if ('set_attribute' in group.actions) setAttribute(elems, group.actions.set_attribute.name, group.actions.set_attribute.value);
+        }
+
         this.entityMap.set(entity.id, {
           state: null,
           appliedClass: null,
           elements: elems,
           classMap: classMap,
-          updateText: (("on_state_change" in group) && ("update_text" in group.on_state_change))?setClass(elems, group.on_state_change.update_text):false,
-          updateRotation: (("on_state_change" in group) && (group.on_state_change.update_rotation === true))
+          updateText: (('actions' in group) && ('update_text' in group.actions))?group.actions.updateText:undefined,
+          updateAttribute: (('actions' in group) && ('update_attribute' in group.actions))?group.actions.updateAttribute:undefined
         });
       });
       
@@ -127,47 +129,64 @@ class ActiveDrawing extends HTMLElement {
     });
 
     this.updateStates();
+  }
 
-    /**
-     * Parse a string of element selectors into an array of DOM
-     * elements. The string must contain a space delimited list
-     * of element ids (beginning with a '#') or class ids (beginning
-     * with a '.').
-     * @param elementIdString - space separated list of element ids.
-     * @returns array of selected DOM elems. 
-     */
-    function getElements(elementIdString) {
-      return(
-        elementIdString.split(/ /).reduce((a,element) => {
-          var elem;
-          switch (element.charAt(0)) {
-            case '#':
-              if (elem = svg_doc.getElementById(element.slice(1))) a.push(elem);
-              break;
-            case '.':
-              Array.prototype.forEach.call(svg_doc.getElementsByClassName(element.slice(1)), (elem) => { a.push(elem); });
-              break;
-            default:
-              break;
-          }
-          return(a);
-        }, [])
-      )
-    }
+  /**
+   * Parse a string of element selectors into an array of DOM
+   * elements. The string must contain a space delimited list
+   * of element ids (beginning with a '#') or class ids (beginning
+   * with a '.').
+   * @param elementIdString - space separated list of element ids.
+   * @returns array of selected DOM elems. 
+   */
+  getElements(elementIdString) {
+    return(
+      elementIdString.split(/ /).reduce((a,element) => {
+        var elem;
+        switch (element.charAt(0)) {
+          case '#':
+            if (elem = svg_doc.getElementById(element.slice(1))) a.push(elem);
+            break;
+          case '.':
+            Array.prototype.forEach.call(svg_doc.getElementsByClassName(element.slice(1)), (elem) => { a.push(elem); });
+            break;
+          default:
+            break;
+        }
+        return(a);
+      }, [])
+    )
+  }
 
-    /**
-     * Assign a CSS class to a collection of DOM elements.
-     * @param {*} elements - array of DOM elements.
-     * @param {*} cssClass - class to be assigned.
-     * @returns true on success false if arguments are invalid.
-     */
-    function setClass(elements, cssClass) {
-      if ((elements.length > 0) && (cssClass !== undefined)) {
-        elements.forEach((element) => { element.classList.add(cssClass); });
-        return(true);
-      }
-      return(false);
+  /**
+   * Assign a CSS class to a collection of DOM elements.
+   * @param {*} elements - array of DOM elements.
+   * @param {*} cssClass - class to be assigned.
+   * @returns true on success false if arguments are invalid.
+   */
+  setClass(elements, cssClass) {
+    if ((elements.length > 0) && (cssClass !== undefined)) {
+      elements.forEach((element) => { element.classList.add(cssClass); });
+      return(true);
     }
+    return(false);
+  }
+
+  setText(elements, text) {
+    if ((elements.length > 0) && (text !== undefined)) {
+      elements.forEach((element) => { element.textContent = text; });
+      return(true);
+    }
+    return(false);
+  }
+
+  setAttribute(elements, attributeName, attributeValue, stateValue = undefined) {
+    if ((elements.length > 0) && (attributeName !== undefined) && (attributeValue !== undefined)) {
+      if (stateValue !== undefined) attributeValue = attributeValue.replace('${state}', stateValue);
+      elements.forEach((element) => { element.setAttribute(attributeName, attributeValue); });
+      return(true);
+    }
+    return(false);
   }
 
   setConfig(config) {
